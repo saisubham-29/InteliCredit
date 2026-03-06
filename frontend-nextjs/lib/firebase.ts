a8dc32f7f8e36ce1dd4bb4a7f42cbdc4e84566f3
SouravLenka <lenkasourav09@gmail.com>
2026-03-06 11:46:10 +0530

    authentication with Google provider.

--- frontend-nextjs/lib/firebase.ts
+++ frontend-nextjs/lib/firebase.ts
@@ -10,7 +10,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 const auth = getAuth(app);
