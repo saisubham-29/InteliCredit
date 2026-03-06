1ba32a3567d4dd8c8f851300bcc38435d33e7b98
Sourav Lenka <lenkasourav09@gmail.com>
2026-03-06 11:38:10 +0530

    Import getAnalytics from firebase/analytics

--- frontend-nextjs/lib/firebase.ts
+++ frontend-nextjs/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
